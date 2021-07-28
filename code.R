06dfbae0ec39b4468adffda6057778987f11241b
Vincenzo Nardelli <vincnardelli@gmail.com>
2021-07-28 10:34:41 +0200

    new code.R with link to open data and vaccines indicators

--- code.R
+++ code.R
@@ -1,60 +1,64 @@
-library(readxl)
 library(dplyr)
-library(here)
-library(writexl)
+library(readxl)
+library(lubridate)
 
-data <- read_excel(here::here("input_indicatore_22luglio - AGGIORNATO.XLSX"))
+url_dpc <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+url_vaccini <- "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv"
+data_dpc <- read.csv(url_dpc)
+data_vaccini <- read.csv(url_vaccini)
+static_data <- read_excel("static_data.xlsx")
 
-names(data)[1:3] <- c("regione", "popolazione", "vaccinati")
+
 efficacia <- 0.95
+delta <- 7
+
+vaccini <- data_vaccini %>% 
+  select(data = data_somministrazione, denominazione_regione = nome_area, fascia_anagrafica, fornitore, prima_dose, seconda_dose, pregressa_infezione) %>% 
+  mutate(data = as_date(data), 
+         denominazione_regione = case_when(denominazione_regione == "Friuli-Venezia Giulia" ~ "Friuli Venezia Giulia", 
+                                           denominazione_regione == "Provincia Autonoma Bolzano / Bozen" ~ "P.A. Bolzano",
+                                           denominazione_regione == "Provincia Autonoma Trento" ~ "P.A. Trento",
+                                           denominazione_regione == "Valle d'Aosta / Vallée d'Aoste" ~ "Valle d'Aosta",
+                                           TRUE ~ denominazione_regione)) %>% 
+  group_by(data, denominazione_regione, fornitore) %>% 
+  summarise_if(is.numeric, sum) %>% 
+  rowwise() %>% 
+  mutate(totale = case_when(fornitore == "Janssen" ~ prima_dose + pregressa_infezione, 
+                            TRUE ~ seconda_dose + pregressa_infezione)) %>% 
+  group_by(data, denominazione_regione) %>% 
+  summarise(nuovi_vaccinati = sum(totale)) %>% 
+  group_by(denominazione_regione) %>% 
+  mutate(vaccinati = cumsum(nuovi_vaccinati))
 
 
-risultati <- data %>%
-  mutate(
-    casi_soglia_50 = popolazione / 100000 * 50,
-    casi_soglia_150 = popolazione / 100000 * 150,
-    casi_soglia_250 = popolazione / 100000 * 250,
-    vaccinati_suscettibili = round(vaccinati * (1 - efficacia), 0),
-    suscettibili = round(popolazione - vaccinati + vaccinati_suscettibili, 0),
-    casi_soglia_50_suscettibili = suscettibili / 100000 * 50,
-    casi_soglia_150_suscettibili = suscettibili / 100000 * 150,
-    casi_soglia_250_suscettibili = suscettibili / 100000 * 250,
-    soglia_50_effettiva = round(casi_soglia_50_suscettibili / (popolazione / 100000), 0),
-    soglia_150_effettiva = round(casi_soglia_150_suscettibili / (popolazione / 100000), 0),
-    soglia_250_effettiva = round(casi_soglia_250_suscettibili / (popolazione / 100000), 0),
-    moltiplicatore_vaccini = casi_soglia_50 / casi_soglia_50_suscettibili,
-    soglia_50_equivalente = round(50 * moltiplicatore_vaccini, 0),
-    soglia_150_equivalente = round(150 * moltiplicatore_vaccini, 0),
-    soglia_250_equivalente = round(250 * moltiplicatore_vaccini, 0),
-    indicatore_soglia_gialla = (incidenza) / soglia_50_equivalente
-  )
+output <- data_dpc %>% 
+  select(data, denominazione_regione, totale_casi, terapia_intensiva, ricoverati_con_sintomi) %>% 
+  mutate(data=as_date(data)) %>% 
+  group_by(denominazione_regione) %>% 
+  mutate(totale_casi_lag = lag(totale_casi, n = delta), 
+         incremento = totale_casi-totale_casi_lag) %>% 
+  ungroup() %>% 
+  left_join(static_data) %>% 
+  left_join(vaccini) %>% 
+  mutate(incidenza = incremento/(popolazione/100000), 
+         saturazione_ti = terapia_intensiva/PL_terapia_intensiva, 
+         saturazione_area_non_critica = ricoverati_con_sintomi/PL_area_non_critica,
+          casi_soglia_50 = popolazione / 100000 * 50,
+          casi_soglia_150 = popolazione / 100000 * 150,
+          casi_soglia_250 = popolazione / 100000 * 250,
+          vaccinati_suscettibili = round(vaccinati * (1 - efficacia), 0),
+          suscettibili = round(popolazione - vaccinati + vaccinati_suscettibili, 0),
+          casi_soglia_50_suscettibili = suscettibili / 100000 * 50,
+          casi_soglia_150_suscettibili = suscettibili / 100000 * 150,
+          casi_soglia_250_suscettibili = suscettibili / 100000 * 250,
+          soglia_50_effettiva = round(casi_soglia_50_suscettibili / (popolazione / 100000), 0),
+          soglia_150_effettiva = round(casi_soglia_150_suscettibili / (popolazione / 100000), 0),
+          soglia_250_effettiva = round(casi_soglia_250_suscettibili / (popolazione / 100000), 0),
+          moltiplicatore_vaccini = casi_soglia_50 / casi_soglia_50_suscettibili,
+          soglia_50_equivalente = round(50 * moltiplicatore_vaccini, 0),
+          soglia_150_equivalente = round(150 * moltiplicatore_vaccini, 0),
+          soglia_250_equivalente = round(250 * moltiplicatore_vaccini, 0),
+          indicatore_stress = (incidenza) / soglia_50_equivalente) %>% 
+  mutate_if(is.numeric, round, digits=2) 
 
-input <- risultati %>%
-  select(regione, popolazione, vaccinati, incidenza)
-
-suscettibili <- risultati %>%
-  select(regione, popolazione, suscettibili)
-
-soglia_effettiva <- risultati %>%
-  select(regione, soglia_50_effettiva, soglia_150_effettiva, soglia_250_effettiva)
-
-soglia_equivalente <- risultati %>%
-  select(regione, moltiplicatore_vaccini, soglia_50_equivalente, soglia_150_equivalente, soglia_250_equivalente)
-
-
-rischio_zona_gialla <- risultati %>%
-  select(regione, vaccinati, incidenza, soglia_50_equivalente, indicatore_soglia_gialla)
-
-
-write_xlsx(
-  list(
-    input = input,
-    suscettibili = suscettibili,
-    soglia_effettiva = soglia_effettiva,
-    soglia_equivalente = soglia_equivalente,
-    rischio_zona_gialla = rischio_zona_gialla,
-    all = risultati
-  ),
-  paste0("data/risultati-",Sys.Date(),"-",sample(10,1),".xlsx")
-)
-
+output
